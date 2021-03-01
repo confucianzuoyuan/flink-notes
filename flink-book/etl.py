b94381bdc2ac36a0d66376033746f18b35b3f314
confucianzuoyuan <zuoyuantc@126.com>
2021-03-01 17:01:32 +0800

    update

---
+++ flink-book/etl.py
@@ -0,0 +1,8 @@
+with open("flink-tutorial.tex", "rt") as fin:
+    with open("out.tex", "wt") as fout:
+        for line in fin:
+            if line.startswith('### '):
+                newline = '\section{' + line.split()[1] + '}'
+                fout.write(newline)
+            else:
+                fout.write(line)
