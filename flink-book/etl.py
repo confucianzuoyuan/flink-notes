b56d95abda933c3cf7fa98613745862dab431bc5
confucianzuoyuan <zuoyuantc@126.com>
2021-03-01 17:18:33 +0800

    update

--- flink-book/etl.py
+++ flink-book/etl.py
@@ -1,8 +1,8 @@
 with open("flink-tutorial.tex", "rt") as fin:
     with open("out.tex", "wt") as fout:
         for line in fin:
-            if line.startswith('### '):
-                newline = '\section{' + line.split()[1] + '}\n'
+            if line.startswith('![](images'):
+                newline = '\\begin{figure}[htbp]\n\centering\n\includegraphics[width=0.6\\textwidth]{' + line.split('![](')[1][:-2] + '}\n\end{figure}\n'
                 fout.write(newline)
             else:
                 fout.write(line)
