24ed6b2939e7b20fc83d29605034a9e5c3b9edb6
confucianzuoyuan <zuoyuantc@126.com>
2021-03-01 17:04:49 +0800

    udpate

--- flink-book/etl.py
+++ flink-book/etl.py
@@ -2,7 +2,7 @@ with open("flink-tutorial.tex", "rt") as fin:
     with open("out.tex", "wt") as fout:
         for line in fin:
             if line.startswith('### '):
-                newline = '\section{' + line.split()[1] + '}'
+                newline = '\section{' + line.split()[1] + '}\n'
                 fout.write(newline)
             else:
                 fout.write(line)
